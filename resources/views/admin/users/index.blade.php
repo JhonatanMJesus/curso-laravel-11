3de35c7e2cb830700e45f75308fbd7e120931b17
Jhonatan <jhonatanjau98@gmail.com>
2025-01-16 22:53:36 -0300

    breadcrumb modify

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @include('admin.users.partials.breadcrumb')
     <div class="py-1 mb-4">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray:400">
+        <h2 class="font-semibold text-xl text-white dark:text-blue">
             Usuários
         </h2>
 
